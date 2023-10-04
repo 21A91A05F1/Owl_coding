7d7df1a58d3e0d6a8b8204c4d841b07dd23dccb7
Naga Sathwika Kovvuri <97424376+21A91A05F1@users.noreply.github.com>
2023-10-04 23:20:11 +0530

    Create Strong Element.py

---
+++ Strong Element.py
@@ -0,0 +1,17 @@
+n=int(input())
+l=list(map(int,input().split()))
+p=[]
+for i in set(l):
+    p.append(l.count(i))
+m=max(p)
+c=0
+res=0
+for i in set(l):
+    if(l.count(i)==m):
+        c+=1
+        res=i
+if(c==1):
+    print("Yes")
+    print(res)
+else:
+    print("No")
